293ba1173d014ff40c0e0b01e67d1c8ff8a01212
AndreFVV <72386531+AndreFVV@users.noreply.github.com>
2021-05-20 19:42:07 +0200

    Update workingDirectories.R

--- workingDirectories.R
+++ workingDirectories.R
@@ -5,9 +5,9 @@ if(Sys.info()['user'] == 'pidu') { ## Pierre
   figDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Figures'            
   tableDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Tables'            
 } else if(Sys.info()['user'] == 'anva') {## Andrea
-  dataDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/02_Data'  
-  analysisDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/03_Analysis'
-  simDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/04_Simulation'
-  figDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Figures'            
-  tableDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Tables'    
+  dataDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/02_Data'  
+  analysisDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/03_Analysis'
+  simDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/04_Simulation'
+  figDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/05_Paper/Figures'            
+  tableDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/05_Paper/Tables'    
 } else stop('unknown user')
