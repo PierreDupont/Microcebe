054698f230fc422538e19bf91a71d564d894960c
AndreFVV <72386531+AndreFVV@users.noreply.github.com>
2021-05-20 19:42:32 +0200

    Update workingDirectories.R

--- workingDirectories.R
+++ workingDirectories.R
@@ -4,7 +4,7 @@ if(Sys.info()['user'] == 'pidu') { ## Pierre
   simDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/04_Simulation'
   figDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Figures'            
   tableDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Tables'            
-} else if(Sys.info()['user'] == 'anva') {## Andrea
+} else if(Sys.info()['user'] == 'anvargas') {## Andrea
   dataDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/02_Data'  
   analysisDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/03_Analysis'
   simDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/04_Simulation'
