a940cb644177e6ae38e7a441d079e0a0212e90d4
Pierre Dupont <pierre.dupont@nmbu.no>
2021-05-20 12:47:34 +0200

    Updated the workingDirectories.R file

--- workingDirectories.R
+++ workingDirectories.R
@@ -1,8 +1,13 @@
-if(Sys.info()['user'] == 'pidu') {
-  dataDir <- 'C:\Users\pidu\Dropbox (Personal)\Mouse lemur CMR data\02_Data'  
-  figDir <- 'C:\Users\pidu\Dropbox (Personal)\Mouse lemur CMR data\02_Data'            ## Pierre
-  dataDir <- 'C:\Users\pidu\Dropbox (Personal)\Mouse lemur CMR data\02_Data'            ## Pierre
-  ## Pierre
-} else if(Sys.info()['user'] == 'cymi') {
-  dataDir <- 'C:/Personal_Cloud/OneDrive/Work/nimbleSCR/'   ## Andrea
+if(Sys.info()['user'] == 'pidu') { ## Pierre
+  dataDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/02_Data'  
+  analysisDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/03_Analysis'
+  simDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/04_Simulation'
+  figDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Figures'            
+  tableDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Tables'            
+} else if(Sys.info()['user'] == 'anva') {## Andrea
+  dataDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/02_Data'  
+  analysisDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/03_Analysis'
+  simDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/04_Simulation'
+  figDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Figures'            
+  tableDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Tables'    
 } else stop('unknown user')
