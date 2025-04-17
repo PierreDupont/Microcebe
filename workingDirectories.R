c6900316a670b5c18da9dcc13058c94be01f25af
Andrea F. Vallejo Vargas <andrea.f.vallejo.vargas@nmbu.no>
2025-04-17 11:45:18 +0200

    Andrea new directory

--- workingDirectories.R
+++ workingDirectories.R
@@ -6,10 +6,17 @@ if(Sys.info()['user'] == 'pidu') { ## Pierre
   tableDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/05_Paper/Tables'
   resultDir <- 'C:/Users/pidu/Dropbox (Personal)/Mouse lemur CMR data/06_Results'
 } else if(Sys.info()['user'] == 'anvargas') {## Andrea
-  dataDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/02_Data'  
-  analysisDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/03_Analysis'
-  simDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/04_Simulation'
-  figDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/05_Paper/Figures'            
-  tableDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/05_Paper/Tables'  
-  resultDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/06_Results'
+  # dataDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/02_Data'  
+  # analysisDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/03_Analysis'
+  # simDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/04_Simulation'
+  # figDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/05_Paper/Figures'            
+  # tableDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/05_Paper/Tables'  
+  # resultDir <- 'C:/Users/anvargas/Dropbox/Mouse lemur CMR data/06_Results'
+  
+  dataDir <- 'C:/Users/anvargas/OneDrive - Norwegian University of Life Sciences/Documents/Collaboration/Mandean/Mouse lemur CMR data/02_Data'
+  analysisDir <- 'C:/Users/anvargas/OneDrive - Norwegian University of Life Sciences/Documents/Collaboration/Mandean/Mouse lemur CMR data/03_Analysis'
+  simDir <- 'C:/Users/anvargas/OneDrive - Norwegian University of Life Sciences/Documents/Collaboration/Mandean/Mouse lemur CMR data/04_Simulation'
+  figDir <- 'C:/Users/anvargas/OneDrive - Norwegian University of Life Sciences/Documents/Collaboration/Mandean/Mouse lemur CMR data/05_Paper/Figures'
+  tableDir <- 'C:/Users/anvargas/OneDrive - Norwegian University of Life Sciences/Documents/Collaboration/Mandean/Mouse lemur CMR data/05_Paper/Tables'
+  resultDir <- 'C:/Users/anvargas/OneDrive - Norwegian University of Life Sciences/Documents/Collaboration/Mandean/Mouse lemur CMR data/06_Results'
 } else stop('unknown user')
